339ad239761025a620f0ea1cd3c0a588e21a2d70
Wojtek <195132@edu.p.lodz.pl>
2018-10-14 12:29:37 +0200

    Dodane przegladanie zdjec

--- Program.py
+++ Program.py
@@ -3,7 +3,12 @@ import ImageDislayer as ImDisp
 
 
 trainImages, trainLabels, testImages, testLabels = ImRead.LoadNumbersImages()
-ImDisp.DisplayNumberImage(trainImages[10])
+#ImDisp.DisplayNumberImage(trainImages[10])
+
+imDisp = ImDisp.ImageDisplayer(6)
+imDisp.DisplayNumberImagesAnimation(trainImages, trainLabels)
+
+#ImDisp.DisplayNumberImagesAnimation(trainImages, trainLabels, 6)
 
 #print(trainLabels[10])
 
